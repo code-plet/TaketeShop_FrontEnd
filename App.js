0d896efec43c7e95146bba2eb63804aa28c89ea3
eNViDAT-student <nvdat.ngvan@gmail.com>
2022-06-19 00:53:25 +0700

    Basic Setup

---
+++ App.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk';
+import {LogBox, StatusBar} from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator.js';
+import productReducer from './src/store/reducers/products';
+import authReducer from './src/store/reducers/auth';
+import bannerReducer from './src/store/reducers/banner';
+import commentReducer from './src/store/reducers/comment';
+import cartReducer from './src/store/reducers/cart';
+import addressReducer from './src/store/reducers/address';
+import orderReducer from './src/store/reducers/order';
+
+LogBox.ignoreLogs(['EventEmitter.removeListener']);
+
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  auth: authReducer,
+  banner: bannerReducer,
+  comment: commentReducer,
+  address: addressReducer,
+  cart: cartReducer,
+  order: orderReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
+function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <StatusBar style="auto" animated={true} showHideTransition={'fade'} />
+      <AppNavigator></AppNavigator>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/constants/NavigatorIndex.js
@@ -0,0 +1,48 @@
+export const HOME_NAVIGATOR = "HomeNavigator";
+export const SHOP_NAVIGATOR = "ShopNavigator";
+export const CART_NAVIGATOR = "CartNavigator";
+export const ORDER_NAVIGATOR = "OrderNavigator";
+export const ACCOUNT_NAVIGATOR = "AccountNavigator";
+export const LOGIN_NAVIGATOR = "LoginNavigator";
+export const PROFILE_NAVIGATOR = "ProfileNavigation";
+//export const PROFILE_NAVIGATOR_FOR_ADMIN = "ProfileNavigation";
+export const PRODUCT_NAVIGATOR = "ProductNavigator";
+
+export const STARTUP_SCREEN = "StartupScreen";
+export const PAYMENT_SCREEN = "PaymentScreen";
+
+
+export const LOGIN_MAIN_SCREEN = "LoginScreen";
+export const FORGOT_PASSWORD_SCREEN = "ForgotPassword";
+export const HOME_MAIN_SCREEN = "HomeMainScreen";
+export const SHOP_MAIN_SCREEN = "ShopMainScreen";
+export const CART_MAIN_SCREEN = "CartMainScreen";
+export const ACCOUNT_MAIN_SCREEN = "AccountMainScreen";
+export const PRODUCT_DETAIL_SCREEN = "ProductDetailScreen";
+export const COMMENT_SCREEN = "CommentScreen";
+export const ADD_COMMENT_SCREEN = "AddCommentDetailScreen";
+export const CATEGORY_DETAIL_SCREEN = "CategoryDetailScreen";
+export const ADDRESS_SCREEN ="AddressScreen";
+export const ADD_ADDRESS_SCREEN ="AddAddressScreen";
+export const SUCCESS_SCREEN ="SuccessScreen";
+
+export const ADD_PRODUCT ="AddProductScreen";
+export const FIX_PRODUCT ="FixProduct";
+
+export const ORDER_WAITING_PAGE ="OrderWaitingPage";
+export const ORDER_CONFIRM_PAGE ="OrderConfirmPage";
+export const ORDER_DELIVERING_PAGE ="OrderDeliveringPage";
+export const ORDER_DELIVERED_PAGE ="OrderDeliveredPage";
+export const ORDER_CANCEL_PAGE ="OrderCancelPage";
+export const ORDER_DETAIL_SCREEN ="OrderDetailScreen";
+
+export const BANNER_SCREEN ="BannerScreen";
+export const LIST_BANNER ="ListBanner";
+export const FIX_BANNER ="FixBanner";
+export const DISCOUNT_SCREEN ="DiscountScreen";
+export const LIST_DISCOUNT ="ListDiscount";
+export const FIX_DISCOUNT ="FixDiscount";
+
+
+export const BOTTOM_BAR_NAVIGATOR = "BottomBarNavigator";
+export const ADMIN_NAVIGATOR = "AdminNavigator";
